4a61c55b9ae5633bf177db28d66955182d9fafc9
cyakuz <cyakuz@gmail.com>
2023-07-14 17:37:58 +0300

    turnjs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import "./style.css";
 import { BrowserRouter } from 'react-router-dom';
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Pejmurde from "./scenes/Pejmurde";
-
+import Issue from "./scenes/Sayilar";
+import "./style.css";
 
 export default function App() {
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Pejmurde/>} />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Pejmurde />} />
+          <Route path="/dergi/:sayiid" element={<Issue />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
