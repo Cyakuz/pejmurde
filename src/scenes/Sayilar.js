40834dbb13c435f3f88fffacaf68a1022fa535b9
Cyakuz <102320084+Cyakuz@users.noreply.github.com>
2023-07-17 17:48:34 +0300

    multiple issue with use params

--- src/scenes/Sayilar.js
+++ src/scenes/Sayilar.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import $ from "jquery";
 import "turn.js";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import Turn from "./Turn.js";
 
@@ -21,8 +21,16 @@ const options = {
   }
 };
 
-const pages = [
+const pages_1 = [
   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/01.jpg",
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/02.jpg",
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/03.jpg",
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/04.jpg",
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/05.jpg",
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/06.jpg"
+];
+
+const pages_2 = [
   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/02.jpg",
   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/03.jpg",
   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/04.jpg",
@@ -32,31 +40,39 @@ const pages = [
 
 const Issue = () => {
   const navigate = useNavigate();
+  const { sayiid } = useParams();
+
+  let pages = [];
+  if (sayiid === '1') {
+    pages = pages_1;
+  } else if (sayiid === '2') {
+    pages = pages_2;
+  }
+
   return (
-  <>
-  <div className="bg">
-  <div class="card">
-  <div>
-  <Turn options={options} className="magazine">
-      {pages.map((page, index) => (
-        <div key={index} className="page">
-          <img src={page} alt="" />
+    <>
+      <div className="bg">
+        <div className="card">
+          <div>
+            <Turn options={options} className="magazine">
+              {pages.map((page, index) => (
+                <div key={index} className="page">
+                  <img src={page} alt="" />
+                </div>
+              ))}
+            </Turn>
+          </div>
+          <div className="card__content">
+            <time datetime="2021-03-30" className="card__date">Tarih</time>
+            <span className="card__title">Pejmürde Dergisi Sayi {sayiid}: Açıklamalar</span>
+            <div className="exit-icon">
+              <i className="fa-solid fa-arrow-right-from-bracket fa-lg" onClick={() => navigate('/')}></i>
+            </div>
+          </div>
         </div>
-      ))}
-    </Turn>
-    </div>
-  <div class="card__content">
-    <time datetime="2021-03-30" class="card__date">Tarih</time>
-    <span class="card__title">Pejmürde Dergisi Sayi 1: Açıklamalar</span> 
-    <div className="exit-icon">
-    <i class="fa-solid fa-arrow-right-from-bracket fa-lg" onClick={() => navigate('/')}></i>
       </div>
-  </div>
-  </div>
-</div>
-  </>
+    </>
   );
 };
 
 export default Issue;
-
